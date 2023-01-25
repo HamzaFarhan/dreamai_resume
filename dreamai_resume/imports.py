0c3925d306c8fa1345c600e4316a90645b9c3b33
HamzaFarhan <thehamza96@gmail.com>
2023-01-25 05:53:57 +0500

    0.1.0

---
+++ dreamai_resume/imports.py
@@ -0,0 +1,7 @@
+# pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
+
+import easyocr
+import wordninja
+from pdf2image import convert_from_path
+
+from dreamai.imports import *
